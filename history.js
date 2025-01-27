2c22185213d96e147c0b635b4df9646b7bb19591
arurion <arurion766@gmail.com>
2025-01-27 14:05:39 +0900

    Update history.js

--- history.js
+++ history.js
@@ -1,7 +1,7 @@
 function history1() {
     if (youtube === true) {
         setInterval(() => {
-            for (youtube===true) { 
+            while (youtube===true) { 
                 history.pushState(null, null, "#" + Math.random());
             }
         }, 1); // 1ミリ秒は非常に短いです。必要に応じてこの値を調整してください。
